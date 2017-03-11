20ac2295c61fbd6b2cbf588d20fc99a8b5742c8b
chevyng <seasoneva@gmail.com>
2017-03-11 17:37:52 +0000

    Testing deploy script

--- application/controller/circle_controller.php
+++ application/controller/circle_controller.php
@@ -86,6 +86,7 @@ class CircleController
         $_SESSION['message'] = 'Circle created!';
         $name = $model->find_circleID_by_name($circle_name);
         $addUser = $model->add_circle_member($name->circleID,$this->current_userID);
+        $_SESSION['message'] = 'Member added!';
       } else {
         $_SESSION['message'] = 'Fail to create circle!';
       }
