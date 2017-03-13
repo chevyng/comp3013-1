2d8284d8ffe5c562737a767a3bcdcbb91b01ee79
chevyng <seasoneva@gmail.com>
2017-03-13 12:13:29 +0000

    Error fixed

--- application/model/circle.php
+++ application/model/circle.php
@@ -129,7 +129,7 @@ class Circle extends Model
 
   public function remove_circle_member($circleID, $userID)
   {
-    $sql = "DELETE FROM circlFfriends
+    $sql = "DELETE FROM circleFriends
             WHERE circleID = :circleID
             AND userID = :userID";
 
